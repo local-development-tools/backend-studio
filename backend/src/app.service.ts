76e3f92bfb8089db1b1b14b9da6653ac871d93b9
lstrycharczuk <148431217+lstrycharczuk@users.noreply.github.com>
2026-04-08 12:45:18 +0200

    Feat/select logs in ai helper (#3)
    * feat: add option for selecting logs in logs AI helper
    
    * fix: app tests

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! You can find the API documentation at <a href="https://github.com/local-development-tools/backend-studio/blob/main/backend/docs/endpointsList.md" target="_blank">here</a>.';
   }
 }
